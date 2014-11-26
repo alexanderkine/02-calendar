d4f18028f243e2fc2d131887a2401997aafc6f40
Alexander Kine <sanya_179510@mail.ru>
2014-11-26 11:42:50 +0500

    Добавил визуализацию, изображение генерируется в папке Debug исходного приложения

---
+++ MyCalendar/MyCalendar/Visualisation.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace MyCalendar
+{
+    partial class Visualisation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.date = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(56, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(292, 72);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Построить страницу календаря";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label
+            // 
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label.Location = new System.Drawing.Point(53, 55);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(295, 87);
+            this.label.TabIndex = 2;
+            this.label.Text = " Введите дату в формате:  \r\n              дд.мм.гггг";
+            // 
+            // date
+            // 
+            this.date.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.date.Location = new System.Drawing.Point(56, 145);
+            this.date.Multiline = true;
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(290, 36);
+            this.date.TabIndex = 3;
+            // 
+            // Visualisation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Visualisation";
+            this.Text = "Visualisation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox date;
+    }
+}
---
+++ MyCalendar/MyCalendar/Visualisation.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyCalendar
+{
+    public partial class Visualisation : Form
+    {
+
+        private Label field;
+
+        public Visualisation()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label.Visible = false;
+            button1.Visible = false;
+            date.Visible = false; 
+            BackColor = Color.White;
+            var dateTime = date.Text;
+            var generator = new CalendarPageGenerator(dateTime);
+            var grid = generator.GenerateDaysGrid();
+            AddMonthAndYearField(generator.GetDate());
+            for (var i = 0; i < grid.GetLength(0); i++)
+            {
+                for (var j = 0; j < grid.GetLength(1); j++)
+                {
+                    if (grid[i, 0] == null)
+                    {
+                        ClientSize = new Size(ClientSize.Width, ClientSize.Height-50);
+                        break;
+                    } 
+                    if (generator.GetDate().Day.ToString().Equals(grid[i, j]) && i!=0)
+                    {
+                        AddNewFieldOfGrid(i, j, grid[i, j], Color.Violet);
+                        continue;
+                    }
+                    AddNewFieldOfGrid(i, j, grid[i, j], Color.White);
+                }
+            }
+            var bmp = new Bitmap(Size.Width, Size.Height);
+            DrawToBitmap(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
+            bmp.Save("haha.bmp");
+        }
+
+        private void AddMonthAndYearField(DateTime date)
+        {
+            var monthes = new String[] { "January", "February", "March", "April", "May", "June", "July", "August","September","October","November","December" };    
+            field = new Label
+            {
+                BackColor = Color.White,
+                Font = new Font("Microsoft Sans Serif", 24F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(204))),
+                ForeColor = Color.Black,
+                Size = new Size(ClientSize.Width, ClientSize.Height / 8 + ClientSize.Height / 16),
+                Location = new Point(0, 0),
+                TextAlign = ContentAlignment.MiddleCenter,
+                BorderStyle = BorderStyle.FixedSingle,
+                Text = string.Format("{0},{1}",monthes[date.Month-1],date.Year)
+            };
+            Controls.AddRange(new Control[] { field });
+            var haha = new String[] {"#", "MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};           
+            for (int i = 0; i < 8; i++)
+            {
+                field = new Label
+                {
+                    BackColor = Color.White,
+                    Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, ((byte) (204))),
+                    ForeColor = Color.Black,
+                    Size = new Size(ClientSize.Width / 8, ClientSize.Height / 16),
+                    Location = new Point(i * ClientSize.Width / 8, ClientSize.Height / 8 + ClientSize.Height / 16),
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    BorderStyle = BorderStyle.FixedSingle,
+                    Text = haha[i]
+                };
+                Controls.AddRange(new Control[] {field});
+            }
+        }
+
+        private void AddNewFieldOfGrid(int i, int j,string text,Color backColor)
+        {
+            Color foreColor;
+            if (j == 0)
+                foreColor = Color.Navy;
+            else if (j == 7)
+                foreColor = Color.Red;
+            else
+                foreColor = Color.Black; 
+            field = new Label
+            {
+                BackColor = backColor,
+                Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(204))),
+                ForeColor = foreColor,
+                Size = new Size(ClientSize.Width / 8, ClientSize.Height / 8),
+                Location = new Point(j * ClientSize.Width / 8, ClientSize.Height / 4 + i * ClientSize.Height / 8),
+                TextAlign = ContentAlignment.MiddleCenter,
+                BorderStyle = BorderStyle.FixedSingle,
+                Text = text
+            };
+            Controls.AddRange(new Control[] {field});
+        }
+    }
+}
