14f54844f3a82abb1c3237998c0a821a78a516c1
Alexander Kine <sanya_179510@mail.ru>
2014-11-26 11:41:08 +0500

    Add logic

---
+++ MyCalendar/MyCalendar/CalendarPageGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalendar
+{
+    public class CalendarPageGenerator
+    {
+        private DateTime Date;
+
+        public CalendarPageGenerator(string date)
+        {
+            try
+            {
+                Date = DateTime.Parse(date);       
+            }
+            catch (Exception) {}   
+        }
+
+        public DateTime GetDate()
+        {
+            return Date;
+        }
+        public CalendarPageGenerator(DateTime date)
+        {
+            Date = date;
+        }
+
+        public String[,] GenerateDaysGrid()
+        {
+            var daysGrid = new String[6, 8];
+            var dateCounter = new DateTime(Date.Year, Date.Month, 1);
+            for (var i = 0; i < daysGrid.GetLength(0); i++)
+            {
+                for (var j = 1; j < daysGrid.GetLength(1); j++)
+                {
+                    var flag = ((int)dateCounter.DayOfWeek != 0) ? (int)(dateCounter.DayOfWeek) == j : j == 7;
+                    if (!flag) continue;
+                    daysGrid[i, j] = dateCounter.Day.ToString();
+                    dateCounter = dateCounter.AddDays(1);
+                    if (dateCounter.Month != Date.Month)
+                        break;  
+                }                
+                daysGrid[i, 0] = (dateCounter.DayOfYear / 7).ToString();
+                if (dateCounter.Month != Date.Month)
+                    break;  
+            }
+            return daysGrid;
+        }
+    }
+}
---
+++ MyCalendar/MyCalendar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalendar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Visualisation().ShowDialog();
+        }
+    }
+}
